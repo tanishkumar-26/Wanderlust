c64cb46a595eeb90b155be273d2e256526c9fa16
tanishkumar <tanishprajapati26@gmail.com>
2025-08-02 00:36:55 +0530

    Fix: updated UI and backend logic

--- app.js
+++ app.js
@@ -30,7 +30,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: true }));
 
 main()
     .then(() => {   
@@ -93,6 +92,10 @@ app.use((req, res, next) => {
 app.use('/listings', listingsRouter);
 app.use('/listings/:id/reviews', reviewsRouter);
 app.use('/', userRouter);
+
+app.get('/', (req, res) => {
+  res.redirect('/listings');
+});
 
 app.all('*', (req, res, next) => {
   next(new ExpressError(404, "Page Not Found!"));
